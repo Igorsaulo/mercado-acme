be6754decd5f75432df8a630189c67cdea331e12
igorsaulo <igorsaulo01@gmail.com>
2023-10-23 00:41:21 -0300

    create application

---
+++ java/ecomerce/src/main/java/com/acme/ecomerce/EcomerceApplication.java
@@ -0,0 +1,13 @@
+package com.acme.ecomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomerceApplication.class, args);
+    }
+
+}
